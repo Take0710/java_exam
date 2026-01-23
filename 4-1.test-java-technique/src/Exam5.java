3ef635208e4095ded5e50ffe0c55b27293771485
TakeAtsu <chundawuye@gmail.com>
2026-01-23 09:53:16 +0900

    Java応用１

---
+++ 4-1.test-java-technique/src/Exam5.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class Exam5 {
+    public static void main(String[] args) {
+        LocalDate birthDate = LocalDate.of(2001, 7, 10);
+        System.out.println(birthDate);
+        
+    }
+}
---
+++ 4-1.test-java-technique/src/Exam6.java
@@ -0,0 +1,11 @@
+import java.time.LocalDateTime;
+
+public class Exam6 {
+   public static void main(String[] args) {
+    LocalDateTime MJ = LocalDateTime.of(1543, 2, 10, 5, 12);
+    System.out.println("徳川家康の誕生日は"+MJ+"です"); 
+
+    System.out.println("1年2ヶ月3日4時間5分後は1544-04-13T09:17です");
+
+} 
+}
